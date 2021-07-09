d4188fd09ccf07d457d620580bba717d1f6985da
LuizFel1pe <luizfelipev166@gmail.com>
2021-07-09 09:58:11 -0300

    Rota post e update

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,5 +4,32 @@ module.exports = {
   async index(req, res) {
     const result = await knex('users');
     return res.json(result);
-  } 
+  },
+
+  async create(req, res, next) {
+    try {
+      const { username } = req.body;
+
+      await knex('users').insert(username);
+
+      return res.status(201).send();
+    } catch (error) {
+      next(error);
+    };
+  },
+
+  async update(req, res, next) {
+    try {
+      const { username } = req.body;
+      const { id } = req.params;
+
+      await knex('users')
+        .update({ username })
+        .where({ id });
+
+      return res.send();
+    } catch (error) {
+      next(error);
+    };
+  }
 };
--- src/server.js
+++ src/server.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 
+app.use(express.json());
 app.use(routes);
-// app.use()
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ error: error.message });
+});
+
 
 app.listen(3333, () => console.log('Server is running on port:3333'));
